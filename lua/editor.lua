4f8374d67aa9d936e03fbae84b3f23225b4b7fea
Rafhael Payao <rafaeltmbr@gmail.com>
2026-03-27 23:55:20 -0300

    feat: add auto-save config

--- lua/editor.lua
+++ lua/editor.lua
@@ -80,3 +80,6 @@ vim.o.scrolloff = 99
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Auto-save
+vim.o.autowriteall = true
